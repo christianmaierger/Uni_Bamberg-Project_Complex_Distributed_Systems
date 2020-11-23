436c2d0184806c65b7bcd11065c83c83336a99e3
christianmaierger <c.maier1122@gmail.com>
2020-11-23 23:45:32 +0100

    edit change structure of creating masterthread in main

--- Assignment1/src/main/java/de/uniba/wiai/dsg/pks/assignment1/histogram/threaded/MasterThread.java
+++ Assignment1/src/main/java/de/uniba/wiai/dsg/pks/assignment1/histogram/threaded/MasterThread.java
@@ -49,39 +49,16 @@ public class MasterThread extends Thread{
         return "SequentialHistogramService";
     }
 
-    /**
-     * Scans a directory with the given Code Snippet 2 from the Assignment sheet and
-     * starts the processing of either directories by calling this method again or the
-     * processing of a file by calling method fileprocessing.
-     * Increments the number of processed directories by one and also calls the log-method for finished
-     * directories. Also increments the number of files in the histogram (just files, not processed files).
-     * The number of processed files is considered in the processFile method.
-     *
-     * @param rootDirectory
-     * @param fileExtension
-     */
-    private void processDirectoryLowLevel(String rootDirectory, String fileExtension) throws InterruptedException, IOException {
-        Path folder = Paths.get(rootDirectory);
-        try(DirectoryStream<Path> stream = Files.newDirectoryStream(folder)){
-            for(Path path: stream){
-                if(Thread.currentThread().isInterrupted()){
-                    throw new InterruptedException("Execution has been interrupted.");
-                }
-                if (Files.isDirectory(path)){
-                   
-                } else if (Files.isRegularFile(path)){
-                    incrementNumberOfFiles();
-                    if (path.getFileName().toString().endsWith(fileExtension)){
-                        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-                        processFile(lines);
-                        out.logProcessedFile(path.toString());
-                    }
-                }
-            }
-        } catch (IOException io){
-            throw new IOException( "I/O error occurred while reading folders and files.");
-        }
+   
 
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        MasterThread masterThread = new MasterThread();
+        masterThread.join();
     }
 
 }
--- Assignment1/src/main/java/de/uniba/wiai/dsg/pks/assignment1/histogram/threaded/lowlevel/LowlevelHistogramService.java
+++ Assignment1/src/main/java/de/uniba/wiai/dsg/pks/assignment1/histogram/threaded/lowlevel/LowlevelHistogramService.java
@@ -6,6 +6,12 @@ import de.uniba.wiai.dsg.pks.assignment.model.HistogramServiceException;
 import de.uniba.wiai.dsg.pks.assignment1.histogram.threaded.MasterThread;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class LowlevelHistogramService implements HistogramService {
 	MasterThread masterThread = new MasterThread(1);
@@ -26,6 +32,41 @@ public class LowlevelHistogramService implements HistogramService {
 			throw new HistogramServiceException(exception.getMessage());
 		}
 		return histogram;
+	}
+
+	/**
+	 * Scans a directory with the given Code Snippet 2 from the Assignment sheet and
+	 * starts the processing of either directories by calling this method again or the
+	 * processing of a file by calling method fileprocessing.
+	 * Increments the number of processed directories by one and also calls the log-method for finished
+	 * directories. Also increments the number of files in the histogram (just files, not processed files).
+	 * The number of processed files is considered in the processFile method.
+	 *
+	 * @param rootDirectory
+	 * @param fileExtension
+	 */
+	private void processDirectoryLowLevel(String rootDirectory, String fileExtension) throws InterruptedException, IOException {
+		Path folder = Paths.get(rootDirectory);
+		try(DirectoryStream<Path> stream = Files.newDirectoryStream(folder)){
+			for(Path path: stream){
+				if(Thread.currentThread().isInterrupted()){
+					throw new InterruptedException("Execution has been interrupted.");
+				}
+				if (Files.isDirectory(path)){
+
+				} else if (Files.isRegularFile(path)){
+					incrementNumberOfFiles();
+					if (path.getFileName().toString().endsWith(fileExtension)){
+						List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+						processFile(lines);
+						out.logProcessedFile(path.toString());
+					}
+				}
+			}
+		} catch (IOException io){
+			throw new IOException( "I/O error occurred while reading folders and files.");
+		}
+
 	}
 
 
