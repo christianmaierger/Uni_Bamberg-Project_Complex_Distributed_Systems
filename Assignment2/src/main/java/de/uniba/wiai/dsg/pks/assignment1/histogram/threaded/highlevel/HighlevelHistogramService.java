8eb600215d1352022b172c51722d01046c101be9
Franzi <franziska.maeckel@stud.uni-bamberg.de>
2020-12-16 09:49:56 +0100

    Upload start folder for assignment 2

---
+++ Assignment2/src/main/java/de/uniba/wiai/dsg/pks/assignment1/histogram/threaded/highlevel/HighlevelHistogramService.java
@@ -0,0 +1,65 @@
+package de.uniba.wiai.dsg.pks.assignment1.histogram.threaded.highlevel;
+
+import de.uniba.wiai.dsg.pks.assignment.model.Histogram;
+import de.uniba.wiai.dsg.pks.assignment.model.HistogramService;
+import de.uniba.wiai.dsg.pks.assignment.model.HistogramServiceException;
+import de.uniba.wiai.dsg.pks.assignment.model.Service;
+import de.uniba.wiai.dsg.pks.assignment1.histogram.threaded.shared.MasterThread;
+import net.jcip.annotations.ThreadSafe;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+@ThreadSafe
+public class HighlevelHistogramService implements HistogramService {
+	private volatile boolean ioExceptionThrown;
+
+	public HighlevelHistogramService() {
+		// REQUIRED FOR GRADING - DO NOT REMOVE DEFAULT CONSTRUCTOR
+		// but you can add code below
+	}
+
+	@Override
+	public Histogram calculateHistogram(String rootDirectory, String fileExtension) throws HistogramServiceException {
+		if(!Objects.nonNull(rootDirectory) || !Objects.nonNull(fileExtension)){
+			throw new HistogramServiceException("Neither root directory nor file extension must be null.");
+		}
+		if(rootDirectory.isBlank() || fileExtension.isBlank()){
+			throw new HistogramServiceException("Neither root directory nor file extension must be empty.");
+		}
+		Path rootPath = Paths.get(rootDirectory);
+		if(!Files.exists(rootPath)){
+			throw new HistogramServiceException("Root directory does not exist.");
+		}
+		if(!Files.isDirectory(rootPath)){
+			throw new HistogramServiceException("Root directory must be a directory");
+		}
+
+		ioExceptionThrown = false;
+		Histogram histogram = new Histogram();
+		Thread masterThread = new MasterThread(rootDirectory, fileExtension, histogram, Service.HIGH_LEVEL, 0.3, this);
+
+		try{
+			masterThread.start();
+			masterThread.join();
+		} catch (InterruptedException exception){
+			masterThread.interrupt();
+			throw new HistogramServiceException("Execution has been interrupted.");
+		}
+		if(ioExceptionThrown){
+			throw new HistogramServiceException("IOException occurred while processing of folder.");
+		}
+		return histogram;
+	}
+
+	public void setIoExceptionThrown(boolean value){
+		this.ioExceptionThrown = value;
+	}
+
+	@Override
+	public String toString() {
+		return "HighlevelHistogramService";
+	}
+}
